97e93802273fdaffcc20cc873b01f1c9525d3382
Giovani <giovanigiggio13@gmail.com>
2024-08-31 11:25:38 -0300

    Added cn helper

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -1,9 +1,10 @@
 import { HomeIcon, GamepadIcon, PrizeIcon, RouteIcon, FaceHappyIcon } from "@/components"
 import {cn} from "@/helpers/cn"
 
+type NavbarProps = React.ComponentProps<"nav">
 type NavbarListProps = React.ComponentProps<"ul">;
 type NavbarListItemProps = React.ComponentProps<"li">;
-
+C
 
 const NavbarList = ({children, className, ...props }: NavbarListProps) => {
   return <ul className={cn("my-4 border-t border-indigo-400/20 border-indigo-400", className)} {...props}>
@@ -16,14 +17,14 @@ const NavbarListItem = ({children, className, ...props}: NavbarListItemProps) =>
   className)} {...props}>
     {children}
   </li>
-  );back
+  );
 };
 
 
 
-export const Navbar = () => {
+export const Navbar = ({className, ...props}: NavbarProps) => {
     return (
-        <nav className="flex h-screen flex-col bg-slate-900 border-r border-indigo-400/20 hover:border-indigo-400/40 w-72 p-2 text-slate-300">
+        <nav className={cn("flex h-screen flex-col bg-slate-900 border-r border-indigo-400/20 hover:border-indigo-400/40 w-72 p-2 text-slate-300", className)} {...props}>
           <div className="flex items-center justify-center">
             <img 
               src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTAwjxGP9h9v1FZXW37MhhHsR0_u_rR0UN7Dg&s" 
