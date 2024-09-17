1d7d1c9c492937a2f1d13bac7eceb2405af58bde
Giovani <giovanigiggio13@gmail.com>
2024-09-17 15:59:37 -0300

    Fetching articles from database.

--- prisma/seed/seed.ts
+++ prisma/seed/seed.ts
@@ -1,49 +1,47 @@
-import {PrismaClient} from '@prisma/client';
+import { PrismaClient } from '@prisma/client';
 import articles from '../../src/data/articles.json';
-import {slugfy} from '../../src/helpers/slugfy';
+import { slugfy } from '../../src/helpers/slugfy';
 
 const prisma = new PrismaClient();
 
 const isDev = process.env.NODE_ENV === "development";
 
-async function main () {
-    console.log ("DB sedd");
+async function main() {
+    console.log("DB seed");
     await seedArticles();
 }
 
-async function seedArticles () {
+async function seedArticles() {
 
     if (!isDev) {
         throw new Error("NODE_ENV is not a development environment.");
     }
 
-    // delete all records
     await prisma.article.deleteMany();
-    // truncate
     await prisma.$executeRawUnsafe("DELETE FROM SQLITE_SEQUENCE WHERE name=$1;", "Article");
 
     for (let article of articles) {
         const record = await prisma.article.create({
-                data: {
-                    title: article.title,
-                    slug: slugfy(article.title),
-                    excerpt: article.title,
-                    content: article.content,
-                    image: article.image,
-                    publishAt: new Date(article.publish_date), 
-                },            
+            data: {
+                title: article.title,
+                slug: slugfy(article.title),
+                excerpt: article.excerpt,
+                content: article.content,
+                image: article.image,
+                publishAt: new Date(article.publish_date), 
+            },            
         });
 
-        console.log("*** created", record.id, record.title);
+        console.log("*** created article", record.id, record.title);
     }
 }
 
 main()
     .then(async () => {
-    await prisma.$disconnect();
-})
-.catch(async(e) => {
-    console.log(e);
-    await prisma.$disconnect();
-    process.exit(1);
-})
+        await prisma.$disconnect();
+    })
+    .catch(async (e) => {
+        console.log(e);
+        await prisma.$disconnect();
+        process.exit(1);
+    });
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
-import articles from '@/data/articles.json';
+import Article from './libs/database/Articles';
 import Image from 'next/image';
 
-export default function Home() {
+export default async function Home() {
+  const articles = await Article.get({orderBy: {publishAt: "desc" }, limit:14});
+  const highlightedArticles = articles.slice(0, 4);
+  const listArticles = articles.slice(4);
+  
   return (
     <div className="ml-72">
       <div className="w-full h-[35vh] bg-orange-400 flex-center">
@@ -12,7 +16,7 @@ export default function Home() {
       <div className="pr-9 pl-9 my-6">
         <div className="grid grid-cols-4 gap-4 h-[35vh]">
 
-          {articles.splice(-4).map((article) => {
+          {highlightedArticles.map((article) => {
             return (<div key={article.title} className='flex-center relative overflow-hidden'>
             <div className='h-full w-full'>
               <Image  
@@ -24,7 +28,9 @@ export default function Home() {
                 />
             </div>
             <p className='absolute bottom-0 pb-0 px-2 bg-gradient-to-t 
-            from-slate-900 via-slate-800 to-transparent w-full'>{article.title}</p>
+            from-slate-900 via-slate-800 to-transparent w-full'>
+              {article.title}
+            </p>
             </div>
             );
           })}
@@ -35,7 +41,7 @@ export default function Home() {
           <div className="grid grid-cols-12 gap-4">
             <div className="col-span-8  flex flex-col gap-4">
               
-          {articles.map((article) => {
+          {listArticles.map((article) => {
             return(
               <div key={article.title} className="flex bg-slate-800 rounded-md py-4">
                 <div className='flex items-center'>
@@ -52,7 +58,9 @@ export default function Home() {
                 <div className="w-full flex flex-col gap-2 pl-4">
                   <h2 className="text-sm mb-4 text-indigo-400">{article.title}</h2>
                   <p className="flex-grow">{article.excerpt}</p>
-                  <button className='bg bg-slate-700 hover:bg-indigo-400/40 rounded-lg px-4 py-2 inline max-w-max'>Ler mais</button>
+                  <button className='bg bg-slate-700 hover:bg-indigo-400/40 rounded-lg px-4 py-2 inline max-w-max'>
+                  Ler mais
+                  </button>
                 </div>
               </div>
               );
