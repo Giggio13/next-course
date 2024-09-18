6754129ae0deeb74e0d964f0d70c99d7a0aaad04
Giovani <giovanigiggio13@gmail.com>
2024-09-18 15:55:25 -0300

    Services to get Home Articles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,10 @@
-import Article from './libs/database/Articles';
+import ArticleService from '@/services/Articles';
 import Image from 'next/image';
 
 export default async function Home() {
-  const articles = await Article.get({orderBy: {publishAt: "desc" }, limit:14});
-  const highlightedArticles = articles.slice(0, 4);
-  const listArticles = articles.slice(4);
-  
+  const articles = await ArticleService.getHomeArticles();
+  const latestArticles = await ArticleService.getHomeLatestArticles();
+
   return (
     <div className="ml-72">
       <div className="w-full h-[35vh] bg-orange-400 flex-center">
@@ -16,11 +15,11 @@ export default async function Home() {
       <div className="pr-9 pl-9 my-6">
         <div className="grid grid-cols-4 gap-4 h-[35vh]">
 
-          {highlightedArticles.map((article) => {
+          {latestArticles.data.map((article) => {
             return (<div key={article.title} className='flex-center relative overflow-hidden'>
             <div className='h-full w-full'>
               <Image  
-                className="h-full w-full object-cover transition duration-500 hover:scale-105 rounded-r-lg" 
+                className="h-full w-full object-cover transition duration-500 hover:scale-105" 
                 src={`/assets/images/articles/${article.image}`} 
                 alt={article.title}
                 width={600}
@@ -40,23 +39,23 @@ export default async function Home() {
         <div className="pr-9 pl-9 ">
           <div className="grid grid-cols-12 gap-4">
             <div className="col-span-8  flex flex-col gap-4">
-              
-          {listArticles.map((article) => {
-            return(
-              <div key={article.title} className="flex bg-slate-800 rounded-md py-4">
-                <div className='flex items-center'>
-                  <div className='h-40 rounded-r-lg overflow-hidden'>
-                    <Image 
-                    className="h-full w-full object-cover transition duration-500 hover:scale-105 rounded-r-lg" 
-                    src={`/assets/images/articles/${article.image}`} 
-                    alt={article.title}
-                    width={600}
-                    height={400}
-                    />
-                  </div>
-                </div>
+              <div>
+                {articles.data.map((article) => {
+                  return(
+                    <div key={article.title} className="flex bg-slate-800 rounded-md py-4">
+                      <div className='flex items-center'>
+                        <div className='h-40 rounded-r-lg overflow-hidden'>
+                          <Image 
+                          className="h-full w-full object-cover transition duration-500 hover:scale-105 rounded-r-lg" 
+                          src={`/assets/images/articles/${article.image}`} 
+                          alt={article.title}
+                          width={600}
+                          height={400}
+                          />
+                        </div>
+                      </div>
                 <div className="w-full flex flex-col gap-2 pl-4">
-                  <h2 className="text-sm mb-4 text-indigo-400">{article.title}</h2>
+                  <h2 className="text-xs mb-4 text-indigo-400">{article.title}</h2>
                   <p className="flex-grow">{article.excerpt}</p>
                   <button className='bg bg-slate-700 hover:bg-indigo-400/40 rounded-lg px-4 py-2 inline max-w-max'>
                   Ler mais
@@ -65,9 +64,8 @@ export default async function Home() {
               </div>
               );
           })}
-              
-              
-              
+            <div>Pagination</div>
+           </div>   
               </div>
             <div className="col-span-4 bg-emerald-500">B</div>
           </div>
